7a6831f9ac4a24e514af3831a087952280ed46e6
marshall4mt <rachaphol@moneytable.com>
2018-07-04 16:45:15 +0700

    test loop

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
 
         <NavBar />
-        <Maincontent />
+        <Maincontent test ="my prop on app.js" />
         <Blog />
 
       {/* end app */}
--- src/component/maincontent/index.jsx
+++ src/component/maincontent/index.jsx
@@ -1,19 +1,84 @@
 import React, { Component}  from 'react';
 
 class Maincontent extends Component {
-    render() {
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="alert alert-info" role="alert">
-                this main page
-              </div>
+
+  constructor () {
+
+    super()
+    this.state = {
+      data: [
+        {
+          list: 'กาแฟเย็น'
+        },
+        {
+          list: 'ชานมไข่มุก'
+        }
+      ]
+    }
+
+  }
+
+  render() {
+    return (
+      <div className="container">
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="alert alert-info" role="alert">
+              this main page & {this.props.test}
             </div>
           </div>
         </div>
-      )
-    }
- }
 
- export default Maincontent;
+        <div className="row">
+          <div className="col-md-12">
+            <div className="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="test" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="someting"></input>
+            </div>
+            <button className="btn btn-primary">Add</button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+          <br/>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+              </tr>
+            </thead>
+            <tbody>  
+              {this.state.data.map((item, id) => <TableRow key={id} data={item.list} />)}
+            </tbody>
+          </table>
+
+            
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+
+}
+
+class TableRow extends Component {
+
+  render() {
+    return (
+      <tr>
+        <th></th>
+        <td>{this.props.data}</td>
+      </tr>
+    );
+  }
+
+}
+
+
+
+
+export default Maincontent;
