eb27b73d87726cbc348bfc53540c8a0cd454d726
marshall4mt <rachaphol@moneytable.com>
2018-07-10 17:34:45 +0700

    change tag

--- src/metatag/index.jsx
+++ src/metatag/index.jsx
@@ -8,13 +8,15 @@ class SetMetatags extends Component {
 
         return (
             <MetaTags>
-                <title>TEST TITLE</title>
-                <meta name="description"  content="TEST TEST" />
-                <meta property="og:title" content="TEST TEST" />
-                <meta property="og:image" content="https://www.moneytable.com/static/media/moneytable@3x.00f0c84f.png" />
+                <title>First React Application</title>
+                <meta name="description"  content="เว็บไซต์ทดสอบของกระผม > Me" />
+                <meta property="og:title" content="First React Application" />
+                <meta property="og:image" content="http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png" />
+                <meta property="og:image:width" content="200"/>
+                <meta property="og:image:height" content="200"/>
                 <meta property="og:type"  content="website" />
                 <meta property="og:url"   content={window.location.href} />
-                <meta property="og:description" content="TEST TEST" />
+                <meta property="og:description" content="เว็บไซต์ทดสอบของกระผม > Me" />
             </MetaTags>
         );
     }
